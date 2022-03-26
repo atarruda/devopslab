dbfae9d2d3a8fb7a61f358a25bef852b26db0c67
atarruda <94978545+atarruda@users.noreply.github.com>
2022-03-26 11:41:33 -0300

    Entrega Final

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - Alexandre Arruda - Teste"
+    return "Laboratório Pipeline DevOps - Alexandre Arruda - Entrega Final"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
